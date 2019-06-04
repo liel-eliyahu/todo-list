b1a92b66d6f954c72e37a53716616146136f0c8e
Liel Eliyahu <liel819@gmail.com>
2019-06-04 17:27:51 +0300

    print a list with delete option and partial edit option

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { FormBuilder } from '@angular/forms';
 import { stringify } from '@angular/compiler/src/util';
+import { NgClass } from '@angular/common';
 
 @Component({
   selector: 'app-root',
@@ -9,13 +10,26 @@ import { stringify } from '@angular/compiler/src/util';
 
 export class AppComponent {
   todos = ['smile', 'eat', 'drink'];
+  index: number;
+  doneClass: string;
 
+  addTodo(newTodo) {
+    if (newTodo) {
+      this.todos.push(newTodo);
+    }
+  }
+  check(done, indexOldTodo) {
+    if (done) {
+      console.log('1');
+      this.doneClass = 'strikethrough';
+    }
+  }
+  edit(indexOldTodo, newTodo) {
+    this.todos[indexOldTodo] = newTodo;
+  }
+  delete(indexTodo) {
+    this.todos.splice(indexTodo, 1);
+    console.log(this.todos);
 
-  // addTask(newTask: string) {
-  //   if (newTask) {
-  //     this.value.push(newTask);
-  //     console.log(this.value);
-
-  //   }
-  // }
+  }
 }
